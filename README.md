bcb89f29b2833ea8745e8e547699800cb6d20e62
woodchuckchoi <choiggoggi@gmail.com>
2020-02-06 09:05:47 +0900

    Daemon Service Script Added

---
+++ README.md
@@ -0,0 +1,2 @@
+INSTRUCTIONS: $[current/directory/]:sudo bash generateService.sh
+and follow following instructions from the shell script.
---
+++ generateService.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+read -p 'APPLICATION TO RUN [local/web/was]: ' app
+appPath=${PWD}
+
+service="[Unit]
+Description=SRE Landing Project Application Daemon Service
+After=network.target
+
+[Service]
+Type=Simple
+ExecStart=sudo $appPath/$app/$app
+Restart=always
+"
+
+sudo echo $service >> /etc/systemd/system/landingProject.service
+
+# sudo systemctl start landingProject.service
