c261d983240587d17cc8a87fabf29a6e9653456f
woodchuckchoi <choiggoggi@gmail.com>
2020-02-06 12:39:24 +0900

    bash script modified

--- generateService.sh
+++ generateService.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
 
 read -p 'APPLICATION TO RUN [local/web/was]: ' app
+if [ $app == 'was' ]
+then
+	read -p 'DB Name: ' DBNAME
+	read -p 'DB User Name: ' DBUSERNAME
+	read -p 'DB Password: ' DBPASSWORD
+fi
+
 
 appPath=${PWD}
 systemPath=/etc/systemd/system/landingProject.service
 
 
-sudo echo "[Unit]" >> $systemPath
-sudo echo "Description=SRE Landing Project Application Daemon Service" >> $systemPath
-sudo echo "After=network.target" >> $systemPath
-sudo echo "[Service]" >> $systemPath
-sudo echo "Type=Simple" >> $systemPath
-sudo echo "ExecStart=sudo $appPath/$app/$app" >> $systemPath
-sudo echo "Restart=always" >> $systemPath
+sudo printf "[Unit]\nDescription=SRE Landing Project Application Daemon Service\nAfter=network.target\n[Service]\nType=Simple\nExecStart=$appPath/$app/$app\nRestart=always" >> $systemPath
 
 sudo systemctl start landingProject.service
